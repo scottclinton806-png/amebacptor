d790dcc15e81045e140f05cf9c207eb782660eda
scottclinton806-png <scottclinton806@gmail.com>
2026-05-17 13:57:01 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,6 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) { return ( <html lang="en"><body>{children}</body></html> ) }
+export default function RootLayout({ children }: { children: React.ReactNode }) { return ( <html lang="en">
+  <head>
+  <link rel="manifest" href="/manifest.json" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  </head>
+  <body>{children}</body></html> ) }
